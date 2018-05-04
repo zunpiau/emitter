1c2a310aa5503a4a882ee9b4cfcca08fb9348629
zunpiau <1330456228@qq.com>
2018-05-04 21:07:12 +0800

    :white_check_mark: Updating tests.

--- src/test/java/io/github/zunpiau/emitter/web/controller/UidControllerTest.java
+++ src/test/java/io/github/zunpiau/emitter/web/controller/UidControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+@ActiveProfiles(value = "prod", inheritProfiles = false)
 public class UidControllerTest extends SpringMvcTest {
 
     @Autowired
--- src/test/java/io/github/zunpiau/emitter/web/interceptor/UidInterceptorTest.java
+++ src/test/java/io/github/zunpiau/emitter/web/interceptor/UidInterceptorTest.java
@@ -1,8 +1,10 @@
 package io.github.zunpiau.emitter.web.interceptor;
 
 import io.github.zunpiau.emitter.SpringMvcTest;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -11,15 +13,36 @@ import org.springframework.web.context.WebApplicationContext;
 
 public class UidInterceptorTest extends SpringMvcTest {
 
-    private static final String uid = "illegalUid";
+    private static final String illegalUid = "illegalUid";
+    private static final String legalUid = "legalUid";
     private static final String requestUrl = "/api/emit/";
     @Autowired
     private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @Before
+    public void init() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
     @Test
-    public void preHandle() throws Exception {
-        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-        mockMvc.perform(MockMvcRequestBuilders.get(requestUrl + uid))
+    public void testBadUid() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(requestUrl + illegalUid))
                 .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void testEmptyRequestBody() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(requestUrl + legalUid)
+                .contentType(MediaType.TEXT_PLAIN))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    public void testPreHandle() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post(requestUrl + legalUid)
+                .contentType(MediaType.TEXT_PLAIN)
+                .content("some text"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
